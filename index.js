2e2604843f82e0007eb75fd88364be9b6eff72e7
Geoffrey <geoffrey>
2024-01-16 11:20:28 +0300

    Done with asignment

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+// Challenge 1
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+// Challenge 2
+let random = Math.floor(Math.random() * 10) + 1;
+
+// Challenge 3
+let num3 = 40;
+let num4 = 6;
+let mod = num3 % num4;
+
+// Challenge 4
+let max = Math.max(2, 5, 7, 14, 20);
+
+// Print results for verification
+console.log("Challenge 1:", multiply);
+console.log("Challenge 2:", random);
+console.log("Challenge 3:", mod);
+console.log("Challenge 4:", max);
+
